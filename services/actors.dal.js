25e783cef7535ef5a1d82d57dfd437d154d0a37e
prawstho <peter.rawsthorne@keyin.com>
2022-11-15 13:31:47 -0330

    Added a style sheet

--- services/actors.dal.js
+++ services/actors.dal.js
@@ -2,6 +2,7 @@ const dal = require("./db");
 
 //get all actors.
 var getActors = function() {
+  if(DEBUG) console.log("actors.dal.getActors()");
   return new Promise(function(resolve, reject) {
     const sql = "SELECT first_name, last_name FROM actor;"
     dal.query(sql, [], (err, result) => {
@@ -17,6 +18,7 @@ var getActors = function() {
 };
 
 var getActorByActorId = function(id) {
+  if(DEBUG) console.log("actors.dal.getActorByActorId()");
   return new Promise(function(resolve, reject) {
     const sql = "SELECT first_name, last_name FROM actor WHERE actor_id = $1";
     dal.query(sql, [id], (err, result) => {
